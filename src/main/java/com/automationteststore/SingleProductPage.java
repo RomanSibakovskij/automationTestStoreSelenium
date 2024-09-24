57362cf9610b357686317f359e973368746facc5
RomanSibakovskij <j4023958@gmail.com>
2024-09-24 14:46:28 +0300

    Add empty SingleProductPageSheet page

---
+++ src/main/java/com/automationteststore/SingleProductPage.java
@@ -0,0 +1,14 @@
+package com.automationteststore;
+
+import org.openqa.selenium.*;
+
+public class SingleProductPage extends BasePage{
+
+    
+
+    public SingleProductPage(WebDriver driver) {
+        super(driver);
+    }
+
+
+}
