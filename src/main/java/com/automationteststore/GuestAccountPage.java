72d4d775b9f9c9309b9212ed4c0b1f78c49593a5
RomanSibakovskij <j4023958@gmail.com>
2024-09-26 06:43:10 +0300

    Rename CheckoutPage sheet to GuestAccountPage

---
+++ src/main/java/com/automationteststore/GuestAccountPage.java
@@ -0,0 +1,13 @@
+package com.automationteststore;
+
+import org.openqa.selenium.*;
+
+public class GuestAccountPage extends BasePage{
+
+
+    public GuestAccountPage(WebDriver driver) {
+        super(driver);
+    }
+
+
+}
