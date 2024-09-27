9901fcf2d2ce8b61c685fc790547f22497b8075c
RomanSibakovskij <j4023958@gmail.com>
2024-09-27 10:22:44 +0300

    Add empty InvoicePage sheet for checkout confirmation test

---
+++ src/main/java/com/automationteststore/InvoicePage.java
@@ -0,0 +1,13 @@
+package com.automationteststore;
+
+import org.openqa.selenium.*;
+
+public class InvoicePage extends BasePage{
+
+
+    public InvoicePage(WebDriver driver) {
+        super(driver);
+    }
+
+
+}
