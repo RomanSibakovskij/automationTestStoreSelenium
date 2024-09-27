a16b9cba0dbc90df5a7540487d6c3e9f4c23b071
RomanSibakovskij <j4023958@gmail.com>
2024-09-27 10:15:58 +0300

    Setup confirm latest products addition to checkout test

--- src/test/java/com/automationteststore/CheckoutPageTest.java
+++ src/test/java/com/automationteststore/CheckoutPageTest.java
@@ -20,4 +20,20 @@ public class CheckoutPageTest extends TestMethods{
         productCheckoutConfirmationTest(checkoutPage);
     }
 
+    // Test 012 -> confirm latest products addition to check out test (as a non-registered user)
+    @Test
+    @DisplayName("Confirm Addition Featured Products  To Checkout As A Non-Registered User")
+    @Tag("Confirm_Product_Addition_To_Checkout")
+    @Tag("Confirm_Latest_Products")
+    void confirmLatestProductAdditionToCheckoutTest(){
+        HomePage homePage = new HomePage(driver);
+        addLatestProductsToCartTest(homePage);
+        ShoppingCartPage shoppingCartPage = new ShoppingCartPage(driver);
+        addSelectedProductsToCheckoutTest(shoppingCartPage);
+        GuestAccountPage guestAccountPage = new GuestAccountPage(driver);
+        validGuestAccountCreationTest(guestAccountPage);
+        CheckoutPage checkoutPage = new CheckoutPage(driver);
+        productCheckoutConfirmationTest(checkoutPage);
+    }
+
 }
