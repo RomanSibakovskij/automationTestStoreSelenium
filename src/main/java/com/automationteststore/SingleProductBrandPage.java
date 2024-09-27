6b7e56c7acbc7aa181f07231c7a40c728fb9d58a
RomanSibakovskij <j4023958@gmail.com>
2024-09-27 18:38:50 +0300

    Add empty SingleProductBrandPage sheet

---
+++ src/main/java/com/automationteststore/SingleProductBrandPage.java
@@ -0,0 +1,13 @@
+package com.automationteststore;
+
+import org.openqa.selenium.*;
+
+public class SingleProductBrandPage extends BasePage{
+
+
+    public SingleProductBrandPage(WebDriver driver) {
+        super(driver);
+    }
+
+    
+}
