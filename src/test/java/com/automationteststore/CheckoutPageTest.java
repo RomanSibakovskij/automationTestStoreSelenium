ce823aa36d4a01ff74bdc6fd475870eb72eec58c
RomanSibakovskij <j4023958@gmail.com>
2024-09-27 10:19:07 +0300

    Setup confirm bestseller products addition to checkout test

--- src/test/java/com/automationteststore/CheckoutPageTest.java
+++ src/test/java/com/automationteststore/CheckoutPageTest.java
@@ -22,12 +22,28 @@ public class CheckoutPageTest extends TestMethods{
 
     // Test 012 -> confirm latest products addition to check out test (as a non-registered user)
     @Test
-    @DisplayName("Confirm Addition Featured Products  To Checkout As A Non-Registered User")
+    @DisplayName("Confirm Addition Latest Products To Checkout As A Non-Registered User")
     @Tag("Confirm_Product_Addition_To_Checkout")
     @Tag("Confirm_Latest_Products")
     void confirmLatestProductAdditionToCheckoutTest(){
         HomePage homePage = new HomePage(driver);
         addLatestProductsToCartTest(homePage);
+        ShoppingCartPage shoppingCartPage = new ShoppingCartPage(driver);
+        addSelectedProductsToCheckoutTest(shoppingCartPage);
+        GuestAccountPage guestAccountPage = new GuestAccountPage(driver);
+        validGuestAccountCreationTest(guestAccountPage);
+        CheckoutPage checkoutPage = new CheckoutPage(driver);
+        productCheckoutConfirmationTest(checkoutPage);
+    }
+
+    // Test 013 -> confirm bestseller products addition to check out test (as a non-registered user)
+    @Test
+    @DisplayName("Confirm Addition Bestseller Products To Checkout As A Non-Registered User")
+    @Tag("Confirm_Product_Addition_To_Checkout")
+    @Tag("Confirm_Bestseller_Products")
+    void confirmBestsellersProductAdditionToCheckoutTest(){
+        HomePage homePage = new HomePage(driver);
+        addBestsellersProductsToCartTest(homePage);
         ShoppingCartPage shoppingCartPage = new ShoppingCartPage(driver);
         addSelectedProductsToCheckoutTest(shoppingCartPage);
         GuestAccountPage guestAccountPage = new GuestAccountPage(driver);
