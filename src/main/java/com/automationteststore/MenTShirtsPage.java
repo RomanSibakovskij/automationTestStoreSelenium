8a7030650b0327a35d1d5a02419977e3f4f84c8a
RomanSibakovskij <j4023958@gmail.com>
2024-09-29 10:20:28 +0300

    Add empty MenTShirts page

---
+++ src/main/java/com/automationteststore/MenTShirtsPage.java
@@ -0,0 +1,13 @@
+package com.automationteststore;
+
+import org.openqa.selenium.WebDriver;
+
+public class MenTShirtsPage extends BasePage{
+
+
+    public MenTShirtsPage(WebDriver driver) {
+        super(driver);
+    }
+
+    
+}
