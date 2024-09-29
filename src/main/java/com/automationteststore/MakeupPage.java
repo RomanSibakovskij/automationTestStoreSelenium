8740b8f183f80286a78da242dbcc797488f25b17
RomanSibakovskij <j4023958@gmail.com>
2024-09-29 10:54:22 +0300

    Add empty MakeupPage sheet

---
+++ src/main/java/com/automationteststore/MakeupPage.java
@@ -0,0 +1,13 @@
+package com.automationteststore;
+
+import org.openqa.selenium.WebDriver;
+
+
+
+public class MakeupPage extends BasePage{
+    public MakeupPage(WebDriver driver) {
+        super(driver);
+    }
+
+    
+}
