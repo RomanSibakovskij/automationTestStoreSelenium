021ea23842f93a166086ae38c7a854bb63acbafb
RomanSibakovskij <j4023958@gmail.com>
2024-10-01 15:41:06 +0300

    Add addition of apparel/accessories(t-shirts) products to checkout test(as a registered user)

--- src/test/java/com/automationteststore/CategoriesProductAddToCheckoutRegUserTest.java
+++ src/test/java/com/automationteststore/CategoriesProductAddToCheckoutRegUserTest.java
@@ -9,13 +9,33 @@ public class CategoriesProductAddToCheckoutRegUserTest extends TestMethods{
     @DisplayName("Add Apparel & Accessories Products (Shoes) To Checkout as Registered User")
     @Tag("Apparel_And_Accessories_Shoes_To_Checkout")
     @Tag("Apparel_And_Accessories_To_Checkout")
-    void addApparelAndAccessoriesShoesToCartTest(){
+    void addApparelAndAccessoriesShoesToCheckoutTest(){
         HomePage homePage = new HomePage(driver);
         goToLoginRegisterPageTest(homePage);
         CreateAccountPage createAccountPage = new CreateAccountPage(driver);
         validUserAccountCreationTest(createAccountPage);
         SingleCategoryProductPage singleCategoryProductPage = new SingleCategoryProductPage(driver);
         apparelAccessoriesShoesAddToCartRegUserTest(singleCategoryProductPage);
+        ShoppingCartPage shoppingCartPage = new ShoppingCartPage(driver);
+        addSelectedProductsToCheckoutAsRegUserTest(shoppingCartPage);
+        CheckoutPage checkoutPage = new CheckoutPage(driver);
+        productCheckoutConfirmationTest(checkoutPage);
+        InvoicePage invoicePage = new InvoicePage(driver);
+        verifyTheOrderSubmissionInInvoicePageTest(invoicePage);
+    }
+
+    // Test 024a -> add apparel & accessories products (men t-shirts) to checkout as a registered user
+    @Test
+    @DisplayName("Add Apparel & Accessories Products (T-Shirts) To Checkout as Registered User")
+    @Tag("Apparel_And_Accessories_TShirts_To_Checkout")
+    @Tag("Apparel_And_Accessories_To_Checkout")
+    void addApparelAndAccessoriesTShirtsToCheckoutTest(){
+        HomePage homePage = new HomePage(driver);
+        goToLoginRegisterPageTest(homePage);
+        CreateAccountPage createAccountPage = new CreateAccountPage(driver);
+        validUserAccountCreationTest(createAccountPage);
+        SingleCategoryProductPage singleCategoryProductPage = new SingleCategoryProductPage(driver);
+        apparelAccessoriesTShirtsAddToCartRegUserTest(singleCategoryProductPage);
         ShoppingCartPage shoppingCartPage = new ShoppingCartPage(driver);
         addSelectedProductsToCheckoutAsRegUserTest(shoppingCartPage);
         CheckoutPage checkoutPage = new CheckoutPage(driver);
