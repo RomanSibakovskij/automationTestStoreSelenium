cb230ab33181ecac513faf4191746d931eea477e
RomanSibakovskij <j4023958@gmail.com>
2024-10-03 06:27:31 +0300

    Add addition of haircare(conditioners) products to checkout test(as a registered user)

--- src/test/java/com/automationteststore/CategoriesProductAddToCheckoutRegUserTest.java
+++ src/test/java/com/automationteststore/CategoriesProductAddToCheckoutRegUserTest.java
@@ -304,4 +304,24 @@ public class CategoriesProductAddToCheckoutRegUserTest extends TestMethods{
         verifyTheOrderSubmissionInInvoicePageTest(invoicePage);
     }
 
+    // Test 029 -> add hair care products (Conditioners) to check out as a registered user
+    @Test
+    @DisplayName("Add Haircare Products (Conditioners) To Checkout  as Registered User")
+    @Tag("Haircare_Conditioner_Products_To_Checkout ")
+    @Tag("Haircare_Products_To_Checkout")
+    void addHaircareConditionersToCheckoutTest(){
+        HomePage homePage = new HomePage(driver);
+        goToLoginRegisterPageTest(homePage);
+        CreateAccountPage createAccountPage = new CreateAccountPage(driver);
+        validUserAccountCreationTest(createAccountPage);
+        SingleCategoryProductPage singleCategoryProductPage = new SingleCategoryProductPage(driver);
+        addHaircareConditionersToCart(singleCategoryProductPage);
+        ShoppingCartPage shoppingCartPage = new ShoppingCartPage(driver);
+        addSelectedProductsToCheckoutAsRegUserTest(shoppingCartPage);
+        CheckoutPage checkoutPage = new CheckoutPage(driver);
+        productCheckoutConfirmationTest(checkoutPage);
+        InvoicePage invoicePage = new InvoicePage(driver);
+        verifyTheOrderSubmissionInInvoicePageTest(invoicePage);
+    }
+
 }
